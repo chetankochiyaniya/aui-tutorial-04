8f3b5b33d4d0e1af799608f4a4900f277edcf708
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-03-23 09:24:59 +0530

    update code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 var express = require('express')
 var route = require('./routes')
 var bodyParser =require('body-parser')
-mongoose.connect('mongodb+srv://ckochiyaniya:Test12345@cluster0.33siq.mongodb.net/products?retryWrites=true&w=majority').then(()=>{
+mongoose.connect('mongodb://ckochiyaniya:test12345@cluster0-shard-00-00.lld3n.mongodb.net:27017,cluster0-shard-00-01.lld3n.mongodb.net:27017,cluster0-shard-00-02.lld3n.mongodb.net:27017/?ssl=true&replicaSet=atlas-13nykk-shard-0&authSource=admin&retryWrites=true&w=majority').then(()=>{
     console.log('connected')
 
     app = express();
